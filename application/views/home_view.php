0ccd7d3b1d0b1de346e6260b6da668784a3aa813
troyGH <troynguyen1991@gmail.com>
2016-10-06 23:58:48 -0700

    City, State, or Zip Search enabled

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,9 +4,9 @@
 
 <!-- Body -->
 <div class="container">
-   <input id="pac-input" class="controls" type="text" placeholder="City, State or Zip">
-   <div id="map"></div>
-   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-6CzpsxPQPdiOV_3M0QhATgjyTqO7JQE&callback=initMap" async defer></script>
+  <input id="pac-input" class="controls" type="text" placeholder="City, State, or Zip">
+  <div id="map"></div>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-6CzpsxPQPdiOV_3M0QhATgjyTqO7JQE&libraries=places&callback=initMap" async defer></script>
 </div> <!-- /container -->
 
 <!-- End Body -->
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,53 +1,64 @@
-//Tabbed Forms Script
-$(document).ready( function(){$('#loginlink').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-tab').removeClass('active');
-		$('#login-tab').addClass('active');
-	});
-	$('#registerlink').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-tab').removeClass('active');
-		$('#register-tab').addClass('active');
-	}); });
-
-
-//Google Maps Stuff
+//Google Map Stuff
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 37.3352, lng: -121.8811},
-          zoom: 5
-        });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.3352, lng: -121.8811},
+    zoom: 5
+  });
+  var input = document.getElementById('pac-input');
+  var autocomplete = new google.maps.places.Autocomplete(input);
 
-				var marker = new google.maps.Marker({
-					map:map
-				});
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
+    map.setCenter(pos);
+    map.setZoom(13);
+  });
+  }
 
-				var input = document.getElementById('pac-input');
-				map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+  autocomplete.bindTo('bounds', map);
+  autocomplete.setTypes(['(regions)']);
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
-            map.setCenter(pos);
-												// Set the position of the marker using the place ID and location.
-						marker.setPosition(pos);
-						marker.setVisible(true);
-						map.setZoom(16);
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 
-          }, function() {
-            //Error
-          });
-        } else {
-          // Browser doesn't support Geolocation
-        }
-      }
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
+    }
+
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(18);
+    }
+
+    // Set the position of the marker using the place ID and location.
+    marker.setPlace({
+      placeId: place.place_id,
+      location: place.geometry.location
+    });
+    marker.setVisible(true);
+
+    infowindow.setContent('<div><strong>' + place.formatted_address + '</strong><br>');
+
+    infowindow.open(map, marker);
+
+  });
+}
 
 function confirmDelete() {
 	bootbox.confirm({
@@ -73,7 +84,7 @@ function confirmDelete() {
 	            setTimeout(function() {
 	            	location.href='home/index';
 	            }, delay);
-	            
+
 
 	        }
 	    }
