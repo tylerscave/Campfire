8ce477ab6f819e8325f0391dfab123177d8661ce
troyGH <troynguyen1991@gmail.com>
2016-10-09 15:59:52 -0700

    more modern search bar

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,7 +4,7 @@
 
 <!-- Body -->
 <div class="container">
-  <input id="pac-input" class="controls" type="text" placeholder="City, State, or Zip">
+  <input id="pac-input" class="form-control" type="text" placeholder="City, State, or Zip">
   <div id="map"></div>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-6CzpsxPQPdiOV_3M0QhATgjyTqO7JQE&libraries=places&callback=initMap" async defer></script>
 </div> <!-- /container -->
