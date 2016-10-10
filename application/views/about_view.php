35343b3d1bcf00a0883a0043ba573b73457f1cb0
penguinarmy94 <luisote94@gmail.com>
2016-10-09 21:09:12 -0700

    about page info added

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -9,13 +9,40 @@
 		<div class="col-md-4">
 			<h4>Notification</h4>
 			<p>This is the About Page.</p>
-			<p>About blah blah blah</p>
 		</div>
 		<div class="col-md-8">
 			<h2>Welcome To Campfire!!!</h2>
-			<p>The purpose of this web app is to do</br>
-			super cool stuff with groups and events</p>
-			<p>GET TO WORK!!!</p>
+			<p>
+				Our team believes in bringing people together who have similar interests. </br>
+				We also want those people to have an opportunity to get involved in </br>
+				activities based on those similar interests. This is what we believe a </br>
+				campfire is all about. Campfire allows anyone, whether you are a user or </br>
+				not, to view events happening in your area. </br>
+			</p>
+			<h3>
+				Registered User
+			</h3>
+			<p>
+				1. Create and join any number of groups based on your interests </br>
+				2. View any events in your area </br>
+				3. View any events in an area you search by zipcode or city,state </br>
+				4. Create events </br>
+				5. Manage group-specific events </br>
+			</p>
+			<h3>
+				Guest
+			</h3>
+			<p>
+				1. View any events in your area </br>
+				2. View any events in the area you search for by zip code or city, state </br>
+				3. Create events </br>
+			</p>
+			<h3>
+				Contact Us
+			</h3>
+			<p>
+				Any questions? suggestions? Contact us at <a href = "http://www.gmail.com">teamcampfire@gmail.com</a>.
+			</p>
 		</div>
 	</div>
 </div>
