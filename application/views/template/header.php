266af628fa6d31e98a077d00036cd08a975422fe
troyGH <troynguyen1991@gmail.com>
2016-10-09 22:32:30 -0700

    fixed firefox browser bug - javascript files should be in header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -18,6 +18,11 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap-theme.css"); ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap-theme.min.css"); ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/custom.css"); ?>">
+	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.10.2.js"); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/bootbox.min.js"); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url("assets/js/custom.js"); ?>"></script>
 </head>
 <body>
 
