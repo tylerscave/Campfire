ceb18a1d1997aac49d780a7d3a322c0797bd2b1d
Tyler Jones <tylerjones365@gmail.com>
2016-10-10 23:25:08 -0700

    Started controller for CreateGroup

--- application/controllers/CreateGroup.php
+++ application/controllers/CreateGroup.php
@@ -12,12 +12,20 @@ class CreateGroup extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper(array('url','html'));
-		$this->load->library('session');
+		$this->load->library(array('session', 'form_validation'));
 		$this->load->database();
 		$this->load->model('user_model');
+		if (!$this->session->userdata('login')) {
+			redirect('home/index');
+		}
 	}
 
 	function index() {
+		// set form validation rules
+		$this->form_validation->set_rules('groupName', 'Group Name', 'trim|required|alpha|min_length[1]|max_length[30]|xss_clean');
+		$this->form_validation->set_rules('zip', 'Group Zip Code', 'trim|required|numeric|min_length[5]|max_length[10]|xss_clean');
+		$this->form_validation->set_rules('description', 'Group Description', 'trim|required|alpha|min_length[1]|max_length[200]|xss_clean');
+		
 		// get user information from session data to create basic profile
 		$details = $this->user_model->get_user_by_id($this->session->userdata('uid'));
 		$data['uname'] = $details[0]->user_fname . " " . $details[0]->user_lname;
