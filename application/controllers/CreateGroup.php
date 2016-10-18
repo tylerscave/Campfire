68bea2fc3adbde7440a06faf15ee48bf4ba1f261
Tyler Jones <tylerjones365@gmail.com>
2016-10-18 01:46:34 -0700

    Quick bug fix. Create Group still completegit add application/controllers/CreateGroup.php ! yay!

--- application/controllers/CreateGroup.php
+++ application/controllers/CreateGroup.php
@@ -31,7 +31,9 @@ class CreateGroup extends CI_Controller {
 		$this->form_validation->set_rules('groupName', 'Group Name', 'trim|required|regex_match[#^[a-zA-Z0-9 \'-]+$#]|min_length[1]|max_length[30]|xss_clean');
 		$this->form_validation->set_rules('zip', 'Group Zip Code', 'trim|required|numeric|min_length[5]|max_length[10]|xss_clean');
 		$this->form_validation->set_rules('description', 'Group Description', 'required|max_length[200]|xss_clean');
-		$this->form_validation->set_rules('imageUpload', 'Upload and Image', 'trim|required|xss_clean');
+		if (empty($_FILES['imageUpload']['name'])) {
+			$this->form_validation->set_rules('imageUpload', 'Upload and Image', 'required');
+		}
 		
 		// submit the form and validate
 		if ($this->form_validation->run() == FALSE) {
