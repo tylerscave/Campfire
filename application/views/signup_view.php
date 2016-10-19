6c299dcafbf8a2659668d74ba03a240349d1f3f1
Jorge Aguiniga <jorge.aguiniga94@gmail.com>
2016-10-19 12:34:28 -0700

    added id attribute to buttons and fields in home, login, and signup views

--- application/views/signup_view.php
+++ application/views/signup_view.php
@@ -12,49 +12,49 @@
 
 			<div class="form-group">
 				<label for="name">First Name</label>
-				<input class="form-control" name="fname" placeholder="First Name" type="text" value="<?php echo set_value('fname'); ?>" />
-				<span class="text-danger"><?php echo form_error('fname'); ?></span>
+				<input id = 'fname' class="form-control" name="fname" placeholder="First Name" type="text" value="<?php echo set_value('fname'); ?>" />
+				<span id = 'fname_error' class="text-danger"><?php echo form_error('fname'); ?></span>
 			</div>
 			<div class="form-group">
 				<label for="name">Last Name</label>
-				<input class="form-control" name="lname" placeholder="Last Name" type="text" value="<?php echo set_value('lname'); ?>" />
-				<span class="text-danger"><?php echo form_error('lname'); ?></span>
+				<input id = 'lname' class="form-control" name="lname" placeholder="Last Name" type="text" value="<?php echo set_value('lname'); ?>" />
+				<span id = 'lname_error' class="text-danger"><?php echo form_error('lname'); ?></span>
 			</div>
 			<div class="form-group">
 				<label for="name">Zip Code</label>
-				<input class="form-control" name="zip" placeholder="Zip Code" type="text" value="<?php echo set_value('zip'); ?>" />
-				<span class="text-danger"><?php echo form_error('zip'); ?></span>
+				<input id = 'zip' class="form-control" name="zip" placeholder="Zip Code" type="text" value="<?php echo set_value('zip'); ?>" />
+				<span id = 'zip_error' class="text-danger"><?php echo form_error('zip'); ?></span>
 			</div>
 			<div class="form-group">
 				<label for="email">Email</label>
-				<input class="form-control" name="email" placeholder="Email" type="text" value="<?php echo set_value('email'); ?>" />
-				<span class="text-danger"><?php echo form_error('email'); ?></span>
+				<input id = 'email' class="form-control" name="email" placeholder="Email" type="text" value="<?php echo set_value('email'); ?>" />
+				<span id = 'email_error' class="text-danger"><?php echo form_error('email'); ?></span>
 			</div>
 			<div class="form-group">
 				<label for="subject">Password</label>
-				<input class="form-control" name="password" placeholder="Password" type="password" />
-				<span class="text-danger"><?php echo form_error('password'); ?></span>
+				<input id = 'password' class="form-control" name="password" placeholder="Password" type="password" />
+				<span id = 'password_error' class="text-danger"><?php echo form_error('password'); ?></span>
 			</div>
 			<div class="form-group">
 				<label for="subject">Confirm Password</label>
-				<input class="form-control" name="cpassword" placeholder="Confirm Password" type="password" />
-				<span class="text-danger"><?php echo form_error('cpassword'); ?></span>
+				<input id = 'confirm' class="form-control" name="cpassword" placeholder="Confirm Password" type="password" />
+				<span id = 'confirm_error' class="text-danger"><?php echo form_error('cpassword'); ?></span>
 			</div>
 
             <div class="form-group">
-                <input type="checkbox" name="emailCheckBox" value="yes"> It's ok to send me (very ocassional) emails about the Campfire.<br>
+                <input id = 'recieve_email'  type="checkbox" name="emailCheckBox" value="yes"> It's ok to send me (very ocassional) emails about the Campfire.<br>
             </div>
 
 			<div class="form-group">
-				<button name="submit" type="submit" class="btn btn-info">Signup</button>
-				<button name="cancel" type="reset" class="btn btn-info">Clear</button>
+				<button id = 'bSignup' name="submit" type="submit" class="btn btn-info">Signup</button>
+				<button id = 'bClear'  name="cancel" type="reset" class="btn btn-info">Clear</button>
 			</div>
 			<?php echo form_close(); ?>
 			<?php echo $this->session->flashdata('msg'); ?>
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-4 col-md-offset-4 text-center">
+		<div id = 'already_registered' class="col-md-4 col-md-offset-4 text-center">
 		Already Registered? <a href="<?php echo base_url(); ?>index.php/login">Login Here</a>
 		</div>
 	</div>
