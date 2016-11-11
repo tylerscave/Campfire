449626f6a058a52bfc49e1f683ad02851ffdddd2
StephenPiazza <stephenpiazza@Stephens-MacBook-Pro.local>
2016-11-10 21:34:04 -0800

    Event Page functionality added

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -13,6 +13,8 @@ class Event extends CI_Controller {
 		parent::__construct();
 		$this->load->helper(array('url', 'html'));
 		$this->load->library('session');
+		$this->load->database();
+		$this->load->model('event_model');
 	}
 
 	function index() {
@@ -20,5 +22,59 @@ class Event extends CI_Controller {
 	}
 	function search(){
 		$this->load->view('searchEvents_view');
+	}
+	
+	function display($eventID = NULL){
+		if ($eventID != NULL) {
+			$uid = $this->session->userdata('uid');
+			$arr['eventID'] = $eventID;
+			$data['info'] = $this->event_model->get_event_by_id($eventID);
+			$data['members'] = $this->event_model->get_event_members($eventID);
+			$data['bulletins'] = $this->event_model->get_bulletins($eventID);
+	
+			$member_status = 'nonmember';
+			if ($this->session->userdata('login') == FALSE) {
+				$member_status = 'notlogged';
+			}
+			else if ( $uid == $data['info']['user_id']) {
+				$member_status = 'owner';
+			}
+			else {
+				foreach($data['members'] as $row) {
+					if ($row['user_id'] == $uid) {
+						$member_status="member";
+					}
+				}
+			}
+	
+			$data['status'] = $member_status;
+			if ($data != NULL) {
+				$this->load->view('event_view', $data);
+			} else {
+				redirect('event/search');
+			}
+		} else {
+			redirect('event/search');
+		}
+	
+	
+	}
+	
+	function join_event($eventID = NULL) {
+		if ($eventID != NULL) {
+			$arr['eventID'] = $eventID;
+			$uid = $this->session->userdata('uid');
+			$this->event_model->join_event($uid, $eventID);
+		}
+		redirect('event/display/'.$eventID);
+	}
+	
+	function leave_event($eventID = NULL) {
+		if ($eventID != NULL) {
+			$arr['eventID'] = $eventID;
+			$uid = $this->session->userdata('uid');
+			$this->event_model->leave_event($uid, $eventID);
+		}
+		redirect('event/display/'.$eventID);
 	}
 }
