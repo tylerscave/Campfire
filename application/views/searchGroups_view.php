6cfd14bc1a53412e628151feb5a8ed4ae74ab1ba
troyGH <troynguyen1991@gmail.com>
2016-11-15 18:59:00 -0800

    UI changes. Card added to search group. May affect other layouts.

--- application/views/searchGroups_view.php
+++ application/views/searchGroups_view.php
@@ -10,38 +10,52 @@
 				<input type="search" class="form-control" id="groupQuery" name="groupQuery" placeholder="Enter a city or zip">
 		</div>
 		<div class="form-group">
-			<button type="submit" id="searchButton" class="btn btn-default"><span class="glyphicon glyphicon-search"></span> </button>
+			<button type="submit" id="searchButton" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span> </button>
 		</div>
 
 <br/>
 	<?php echo form_close(); ?>
-	    <div class="container">
-				<div>
-					<button class="btn btn-default filter-button" id="all-btn" data-filter="all">All</button>
-					<button class="btn btn-default filter-button" id="movies-btn" data-filter="Movies">Movies</button>
-					<button class="btn btn-default filter-button" id="education-btn" data-filter="Education">Education</button>
-					<button class="btn btn-default filter-button" id="sports-btn" data-filter="Sports">Sports</button>
-					<button class="btn btn-default filter-button" id="food-btn" data-filter="Food">Food</button>
-					<button class="btn btn-default filter-button" id="coffee-btn" data-filter="Coffee">Coffee</button>
-				</div>
-	        <div class="row no-gutter">
+	    <div class="col-md-10 col-md-offset-1">
 						<?php
 							if(isset($groups)) {
-								echo "<h1>".count($groups)." Group(s) Found:</h1>";
-								displayTiles($groups);
+								echo "<h3>".count($groups)." Group(s) Found:</h3>";
 							}
 							else{
 								if($this->input->get('groupQuery')){
-									echo "<h1>0 Groups Found</h1>";
+									echo "<h3>0 Groups Found</h3></div>";
 								}
 								else{
 									if(isset($random)){
-										echo "<h1>Here's some random groups:</h1>";
-										displayTiles($random);
+										echo "<h5>Here are some random groups:</h5>";
 									}
 								}
 							} ?>
-	        </div>
+							<div class="col-md-1 well" style="min-height: 100px;">
+								<div class="form-group">
+								  <label for="select-tag">Filter:</label>
+								  <select class="form-control" id="select-tag">
+								    <option id="all-opt" data-filter="all">All</option>
+								    <option id="movies-opt" data-filter="Movies">Movies</option>
+								    <option id="education-opt" data-filter="Education">Education</option>
+								    <option id="sports-opt" data-filter="Sports">Sports</option>
+										<option id="food-opt" data-filter="Food">Food</option>
+										<option id="coffe-opt" data-filter="Coffee">Coffee</option>
+								  </select>
+								</div>
+							</div>
+							<div class="col-md-10 well">
+								<div class="card-group">
+							<?php
+								if(isset($groups)) {
+										displayTiles($groups);
+								}else{
+										if(isset($random)){
+											displayTiles($random);
+										}
+									}
+									?>
+							</div>
+						</div>
 	    </div>
 </div>
 <!-- End Body -->
@@ -53,36 +67,33 @@
 <?php
 
 function displayTiles($groups){
-	foreach($groups as $group){
-		echo "<div class='col-md-3 filter ".$group['tag_title']."'>";
-		echo "<p class='org-title'>".$group['org_title']."</p>";
-		echo "<a id='".$group['org_id']."' href='".base_url()."index.php/Group/display/".$group['org_id']."'>";
-		echo "<img class='img-responsive center-cropped' src='";
-		echo base_url()."uploads/".$group['org_picture']."' alt='".$group['org_title']."'></a></div>";
+	for ($x = 0; $x < count($groups); $x++) {
+		echo "<div class='col-md-3 card filter ".$groups[$x]['tag_title']."'>";
+		echo "<a id='".$groups[$x]['org_id']."' href='".base_url()."index.php/Group/display/".$groups[$x]['org_id']."'><img class='img-responsive' src='";
+		echo base_url()."uploads/".$groups[$x]['org_picture']."' alt='".$groups[$x]['org_title']."'></a>";
+		echo "<div class='card-block'>";
+		echo"<h4 class='card-title' id='card-title'>".$groups[$x]['org_title']."</h4>";
+		echo "<p class='card-text'>".$groups[$x]['org_description']."</p>
+		<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Group/display/".$groups[$x]['org_id']."'>See More</a>
+		<p class='card-text'><small class='text-muted'>Last updated 3 mins ago</small></p></div></div>";
 	}
 }
 ?>
 
 <script>
 $(document).ready(function(){
-	var clicked = "btn btn-default filter-button active";
-	var defaultBtn = "btn btn-default filter-button";
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-
-        if(value == "all")
-        {
-            $('.filter').show('1000');
-        }
-        else
-        {
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-
-        }
-				$(".filter-button").removeClass(clicked).addClass(defaultBtn);
-				$('#'+value.toLowerCase()+'-btn').addClass(clicked);
-    });
+	$('#select-tag').on('change', function() {
+    var value = $(this).find(':selected');
+		if(value.val() == "All")
+		{
+				$('.filter').show('1000');
+		}
+		else
+		{
+				$(".filter").not('.'+value.val()).hide('3000');
+				$('.filter').filter('.'+value.val()).show('3000');
+		}
+	});
 
 });
 </script>
