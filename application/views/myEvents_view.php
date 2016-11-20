3294cb2d4e16feedb62d9a2e1e78265a6d1b22e1
penguinarmy94 <luisote94@gmail.com>
2016-11-19 16:27:11 -0800

    Draft of my events. Still trying to work out pagination here

--- application/views/myEvents_view.php
+++ application/views/myEvents_view.php
@@ -7,77 +7,85 @@
 	<div class="row" style="padding-top:50px">
 	<div class="container-fluid">
 		<ul class="nav nav-pills nav-justified" style="background-color:white">
-		<li class="active"><a data-toggle="tab" href="#owned_groups">Owned</a></li>
-		<li><a data-toggle="tab" href="#joined_groups">Joined</a></li>
+			<li class="active"><a data-toggle="tab" href="#owned_events">Owned</a></li>
+			<li><a data-toggle="tab" href="#joined_events">Joined</a></li>
 		</ul>
-
-		<div class="tab-content">
-			<div id="owned_groups" class="tab-pane fade in active">
-			<?php
-				if ($ownedevents == NULL)
-				{
-					echo '<h4 id="create_event_link" style="padding-top:30px">No events created. Create an event? <a href="createEvent/index">click here</a></h4>';
-				}
-				else {
-					echo '<div style="padding-top:30px">';
-					
-						//lists all the events that the user created
-						if ($ownedgroups != null) {
-							$size = sizeof($ownedevents);
-							for ($x = 0; $x < $size; $x++) {
-								echo "<div class='col-md-3 filter ".$ownedevents[$x]->org_tag."'>";
-								echo "<p class='org-title'>".$ownedevents[$x]->org_title."</p>";
-								echo "<a href='".base_url()."index.php/Group/display/".$ownedevents[$x]->org_id."'>";
-								echo "<img class='img-responsive center-cropped' src='";
-								echo base_url()."uploads/".$ownedevents[$x]->org_picture."' alt='".$ownedevents[$x]->org_title."'></a></div>";																																																																																																	
+		<div style="padding-top:50px"></div>
+		<div class="col-md-12 well" id="event_layout">
+			<div class="col-md-10">
+				<div class="tab-content">
+					<div id="owned_events" class="tab-pane fade in active">
+						<?php
+							if ($ownedevents == NULL)
+							{
+								echo '<h4 id="create_event_link" style="padding-top:30px">No events created. Create an event? <a href="createEvent/index">click here</a></h4>';
 							}
-							
-						}
-					echo "</div>";
-				}
-				?>
-			</div>
-			<div id="joined_groups" class="tab-pane fade">
-				<?php
-					if (sizeof($memberedevents) == sizeof($ownedevents))
-					{
-						echo '<h4 id="search_event_link" style="padding-top:30px">No events pending. Search for events to attend: <a href="';
-						echo base_url('index.php/Event/search');
-						echo '">click here</a></h4>';
-					}
-					else
-					{
-						echo '<div style="padding-top:30px">';
-							//list all events the user RSVP
-							if ($memberedevents != null) {
-								$memberedsize = sizeof($memberedevents);
-								$ownedsize = sizeof($ownedevents);
-								$ownedsameasmembered = false;
-								for ($x = 0; $x < $memberedsize; $x++) {
-									//excludes all events created by the user to show up on RSVP list
-									for ($y = 0; $y < $ownedsize; $y++) {
-										if ($memberedevents[$x]->org_id == $ownedevents[$y]->org_id) {
-											$ownedsameasmembered = true;
-										}
-									}
-									if ($ownedsameasmembered != true) {
-										echo "<div class='col-md-3 filter ".$memberedevents[$x]->org_tag."'>";
-										echo "<p class='org-title'>".$memberedevents[$x]->org_title."</p>";
-										echo "<a href='".base_url()."index.php/Group/display/".$memberedevents[$x]->org_id."'>";
-										echo "<img class='img-responsive center-cropped' src='";
-										echo base_url()."uploads/".$memberedevents[$x]->org_picture."' alt='".$memberedevents[$x]->org_title."'></a></div>";
-									}
-									else {
-										$ownedsameasmembered = false;
-									}
+							else {								
+								$ci =& get_instance();
+								$ci->set_config($ownedevents);
+								$per_page = $ci->input->get('per_page');
+								$index = 12 * $per_page;
+								$max = $index + 12;
+								if($max > count($ownedevents)-1){
+									$max = count($ownedevents); //in case of out of range error
 								}
+								for ($x = $index; $x < $max; $x++) {
+									echo "<div class='col-md-3 card'>";
+									echo "<a id='".$ownedevents[$x]->event_id."' href='".base_url()."index.php/Event/display/".$ownedevents[$x]->event_id."'><img class='img-responsive center-cropped' src='";
+									echo base_url()."uploads/".$ownedevents[$x]->event_picture."' alt='".$ownedevents[$x]->event_title."'></a>";
+									echo "<div class='card-block'>";
+									echo"<h4 class='card-title' id='card-title'>".$ownedevents[$x]->event_title."</h4>";
+									echo "<p class='card-text'>".$ownedevents[$x]->event_description."</p>
+									<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Event/display/".$ownedevents[$x]->event_id."'>See More</a>
+									</div></div>";
+								}
+								
+								echo "<div class='col-md-12'>";
+								echo $ci->pagination->create_links();
+								echo "</div>";
+								
 							}
-						echo "</div>";
-					}
-				?>
+						?>
+					</div>
+					<div id="joined_events" class="tab-pane fade">
+						<?php
+							if ($memberedevents == NULL)
+							{
+								echo '<h4 id="search_event_link" style="padding-top:30px">No events pending. Search for events to attend: <a href="';
+								echo base_url('index.php/Event/search');
+								echo '">click here</a></h4>';
+							}
+							else
+							{
+								$ci =& get_instance();
+								$ci->set_config($memberedevents);
+								$per_page = $ci->input->get('per_page');
+								$index = 12 * $per_page;
+								$max = $index + 12;
+								if($max > count($memberedevents)-1){
+									$max = count($memberedevents); //in case of out of range error
+								}										
+								for ($x = $index; $x < $max; $x++) {
+									echo "<div class='col-md-3 card'>";
+									echo "<a id='".$memberedevents[$x]->event_id."' href='".base_url()."index.php/Event/display/".$memberedevents[$x]->event_id."'><img class='img-responsive center-cropped' src='";
+									echo base_url()."uploads/".$memberedevents[$x]->event_picture."' alt='".$memberedevents[$x]->event_title."'></a>";
+									echo "<div class='card-block'>";
+									echo"<h4 class='card-title' id='card-title'>".$memberedevents[$x]->event_title."</h4>";
+									echo "<p class='card-text'>".$memberedevents[$x]->event_description."</p>
+									<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Event/display/".$memberedevents[$x]->event_id."'>See More</a>
+									</div></div>";
+								}
+								
+								echo "<div class='col-md-12'>";
+								echo $ci->pagination->create_links();
+								echo "</div>";
+								
+							}
+						?>
+					</div>
+				</div>		
 			</div>
-		</div>
-	</div>
+	    </div>
 	</div>
 </div>
 <!-- End Body -->
