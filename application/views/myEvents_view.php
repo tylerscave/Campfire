9945537c92372a3eb7eeb77cb548a740d6d2a332
penguinarmy94 <luisote94@gmail.com>
2016-11-23 18:13:23 -0800

    changed color and size of nav-pills. Still need to center them.

--- application/views/myEvents_view.php
+++ application/views/myEvents_view.php
@@ -5,12 +5,14 @@
 <!-- Body -->
 <div class="container custom-body">
 	<div class="row" style="padding-top:50px">
-	<div class="container-fluid">
-		<ul class="nav nav-pills nav-justified" style="background-color:white">
+	<div class="container">
+		<ul class="nav nav-pills">
 			<li class="active"><a data-toggle="tab" href="#owned_events">Owned</a></li>
 			<li><a data-toggle="tab" href="#joined_events">Joined</a></li>
 		</ul>
-		<div style="padding-top:50px"></div>
+	</div>
+	<div class="container-fluid">
+		<div style="padding-top:20px"></div>
 		<div class="col-md-12 well" id="event_layout">
 			<div class="col-md-10">
 				<div class="tab-content">
@@ -21,29 +23,7 @@
 								echo '<h4 id="create_event_link" style="padding-top:30px">No events created. Create an event? <a href="createEvent/index">click here</a></h4>';
 							}
 							else {								
-								$ci =& get_instance();
-								$ci->set_config($ownedevents);
-								$per_page = $ci->input->get('per_page');
-								$index = 12 * $per_page;
-								$max = $index + 12;
-								if($max > count($ownedevents)-1){
-									$max = count($ownedevents); //in case of out of range error
-								}
-								for ($x = $index; $x < $max; $x++) {
-									echo "<div class='col-md-3 card'>";
-									echo "<a id='".$ownedevents[$x]->event_id."' href='".base_url()."index.php/Event/display/".$ownedevents[$x]->event_id."'><img class='img-responsive center-cropped' src='";
-									echo base_url()."uploads/".$ownedevents[$x]->event_picture."' alt='".$ownedevents[$x]->event_title."'></a>";
-									echo "<div class='card-block'>";
-									echo"<h4 class='card-title' id='card-title'>".$ownedevents[$x]->event_title."</h4>";
-									/*echo "<p class='card-text'>".$ownedevents[$x]->event_description."</p>*/
-									echo "<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Event/display/".$ownedevents[$x]->event_id."'>See More</a>
-									</div></div>";
-								}
-								
-								echo "<div class='col-md-12'>";
-								echo $ci->pagination->create_links();
-								echo "</div>";
-								
+								make_tiles("owned", count($ownedevents), $ownedevents);	
 							}
 						?>
 					</div>
@@ -57,23 +37,7 @@
 							}
 							else
 							{
-								$ci =& get_instance();
-								$per_page = $ci->input->get('per_page');
-								$index = 12 * $per_page;
-								$max = $index + 12;
-								if($max > count($memberedevents)-1){
-									$max = count($memberedevents); //in case of out of range error
-								}										
-								for ($x = $index; $x < $max; $x++) {
-									echo "<div class='col-md-3 card'>";
-									echo "<a id='".$memberedevents[$x]->event_id."' href='".base_url()."index.php/Event/display/".$memberedevents[$x]->event_id."'><img class='img-responsive center-cropped' src='";
-									echo base_url()."uploads/".$memberedevents[$x]->event_picture."' alt='".$memberedevents[$x]->event_title."'></a>";
-									echo "<div class='card-block'>";
-									echo"<h4 class='card-title' id='card-title'>".$memberedevents[$x]->event_title."</h4>";
-									/*echo "<p class='card-text'>".$memberedevents[$x]->event_description."</p>*/
-									echo "<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Event/display/".$memberedevents[$x]->event_id."'>See More</a>
-									</div></div>";
-								}
+								make_tiles("membered", count($memberedevents), $memberedevents);
 							}
 						?>
 					</div>
@@ -84,6 +48,43 @@
 </div>
 <!-- End Body -->
 
+
+<?php
+
+function make_tiles($set, $size, $events)
+{
+	if ($set == "owned")
+	{
+		for ($x = 0; $x < $size; $x++) {
+			echo "<div class='col-md-3 card'>";
+			echo "<a id='".$events[$x]->event_id."' href='".base_url()."index.php/Event/display/".$events[$x]->event_id."'><img class='img-responsive center-cropped' src='";
+			echo base_url()."uploads/".$events[$x]->event_picture."' alt='".$events[$x]->event_title."'></a>";
+			echo "<div class='card-block'>";
+			echo"<h4 class='card-title' id='card-title'>".$events[$x]->event_title."</h4>";
+			echo "<p class='card-text'>".$events[$x]->event_description."</p>";
+			echo "<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Event/display/".$events[$x]->event_id."'>See More</a>
+			</div></div>";
+		}
+	}
+	else if ($set == "membered")
+	{
+		for ($x = 0; $x < $size; $x++) {
+			echo "<div class='col-md-3 card'>";
+			echo "<a id='".$events[$x]->event_id."' href='".base_url()."index.php/Event/display/".$events[$x]->event_id."'><img class='img-responsive center-cropped' src='";
+			echo base_url()."uploads/".$events[$x]->event_picture."' alt='".$events[$x]->event_title."'></a>";
+			echo "<div class='card-block'>";
+			echo"<h4 class='card-title' id='card-title'>".$events[$x]->event_title."</h4>";
+			echo "<p class='card-text'>".$events[$x]->event_description."</p>";
+			echo "<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Event/display/".$events[$x]->event_id."'>See More</a>
+			</div></div>";
+		}
+	}
+	else{
+		return;
+	}
+}
+
+?>
 <!-- Footer -->
 <?php $this->load->view('template/footer.php'); ?>
 <!-- End Footer -->
--- application/views/myGroups_view.php
+++ application/views/myGroups_view.php
@@ -4,48 +4,27 @@
 
 <!-- Body -->
 <div class="container custom-body">
-	<div class="row" style="padding-top:50px">
-	<div class="col-md-4" align="center">
-		<ul class="nav nav-pills nav-justified" style="background-color:white">
+	<div class="row">
+	<div class="container" style="padding-top:50px">
+		<ul class="nav nav-pills">
 		<li id="owned_groupsTab" class="active"><a data-toggle="tab" href="#owned_groups">Owned</a></li>
 		<li id="joined_groupsTab"><a data-toggle="tab" href="#joined_groups">Joined</a></li>
 		</ul>
 	</div>
 	<div class="container-fluid">
-		<div style="padding-top:50px"></div>
+		<div style="padding-top:20px"></div>
 		<div class="col-md-12 well" id="group_view_layout">
 			<div class="col-md-10">
 				<div class="tab-content">
 					<div id="owned_groups" class="tab-pane fade in active">
 						<?php
+							
 							if ($ownedgroups == NULL)
 							{
 								echo '<h4 style="padding-top:30px">No groups owned. Create a group? <a href="createGroup/index">click here</a></h4>';
 							}
-							else {								
-								$ci =& get_instance();
-								$ci->set_config($ownedgroups);
-								$per_page = $ci->input->get('per_page');
-								$index = 12 * $per_page;
-								$max = $index + 12;
-								if($max > count($ownedgroups)-1){
-									$max = count($ownedgroups); //in case of out of range error
-								}
-								for ($x = $index; $x < $max; $x++) {
-									echo "<div class='col-md-3 card'>";
-									echo "<a id='".$ownedgroups[$x]->org_id."' href='".base_url()."index.php/Group/display/".$ownedgroups[$x]->org_id."'><img class='img-responsive center-cropped' src='";
-									echo base_url()."uploads/".$ownedgroups[$x]->org_picture."' alt='".$ownedgroups[$x]->org_title."'></a>";
-									echo "<div class='card-block'>";
-									echo"<h4 class='card-title' id='card-title'>".$ownedgroups[$x]->org_title."</h4>";
-									echo "<p class='card-text'>".$ownedgroups[$x]->org_description."</p>
-									<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Group/display/".$ownedgroups[$x]->org_id."'>See More</a>
-									</div></div>";
-								}
-								
-								echo "<div class='col-md-12'>";
-								echo $ci->pagination->create_links();
-								echo "</div>";
-								
+							else {
+								make_tiles("owned", count($ownedgroups), $ownedgroups);
 							}
 						?>
 					</div>
@@ -59,23 +38,7 @@
 							}
 							else
 							{
-								$ci =& get_instance();
-								$per_page = $ci->input->get('per_page');
-								$index = 12 * $per_page;
-								$max = $index + 12;
-								if($max > count($memberedgroups)-1){
-									$max = count($memberedgroups); //in case of out of range error
-								}										
-								for ($x = $index; $x < $max; $x++) {
-									echo "<div class='col-md-3 card'>";
-									echo "<a id='".$memberedgroups[$x]->org_id."' href='".base_url()."index.php/Group/display/".$memberedgroups[$x]->org_id."'><img class='img-responsive center-cropped' src='";
-									echo base_url()."uploads/".$memberedgroups[$x]->org_picture."' alt='".$memberedgroups[$x]->org_title."'></a>";
-									echo "<div class='card-block'>";
-									echo"<h4 class='card-title' id='card-title'>".$memberedgroups[$x]->org_title."</h4>";
-									echo "<p class='card-text'>".$memberedgroups[$x]->org_description."</p>
-									<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Group/display/".$memberedgroups[$x]->org_id."'>See More</a>
-									</div></div>";
-								}
+								make_tiles("membered", count($memberedgroups), $memberedgroups);
 							}
 						?>
 					</div>
@@ -86,6 +49,43 @@
 	</div>
 </div>
 <!-- End Body -->
+
+<?php
+	
+function make_tiles($set, $size, $groups)
+{
+	if ($set == "owned")
+	{
+		for ($x = 0; $x < $size; $x++) {
+			echo "<div class='col-md-3 card'>";
+			echo "<a id='".$groups[$x]->org_id."' href='".base_url()."index.php/Group/display/".$groups[$x]->org_id."'><img class='img-responsive center-cropped' src='";
+			echo base_url()."uploads/".$groups[$x]->org_picture."' alt='".$groups[$x]->org_title."'></a>";
+			echo "<div class='card-block'>";
+			echo"<h4 class='card-title' id='card-title'>".$groups[$x]->org_title."</h4>";
+			echo "<p class='card-text'>".$groups[$x]->org_description."</p>
+			<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Group/display/".$groups[$x]->org_id."'>See More</a>
+			</div></div>";
+		}
+	}
+	else if ($set == "membered")
+	{								
+		for ($x = 0; $x < $size; $x++) {
+			echo "<div class='col-md-3 card'>";
+			echo "<a id='".$groups[$x]->org_id."' href='".base_url()."index.php/Group/display/".$groups[$x]->org_id."'><img class='img-responsive center-cropped' src='";
+			echo base_url()."uploads/".$groups[$x]->org_picture."' alt='".$groups[$x]->org_title."'></a>";
+			echo "<div class='card-block'>";
+			echo"<h4 class='card-title' id='card-title'>".$groups[$x]->org_title."</h4>";
+			echo "<p class='card-text'>".$groups[$x]->org_description."</p>
+			<a class='btn btn-primary waves-effect waves-light' href='".base_url()."index.php/Group/display/".$groups[$x]->org_id."'>See More</a>
+			</div></div>";
+		}
+	}
+	else{
+		return;
+	}
+}
+
+?>
 
 <!-- Footer -->
 <?php $this->load->view('template/footer.php'); ?>
