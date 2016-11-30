fd31f3e2123ec7ad2e0955a6a24c63c7a687254f
Troy Nguyen <root@AirMac.local>
2016-11-29 17:58:43 -0800

    group tiles image fixed

--- application/views/searchGroups_view.php
+++ application/views/searchGroups_view.php
@@ -74,7 +74,7 @@ function displayTiles($groups){
 		$truncatedDesc = strlen($groups[$x]['org_description']) > 18 ? substr($groups[$x]['org_description'], 0, 18).'...' : $groups[$x]['org_description']."<br><br>";
 		$truncatedTitle = strlen($groups[$x]['org_title']) > 50 ? substr($groups[$x]['org_title'], 0, 50).'...' : $groups[$x]['org_title'];
 		echo "<div class='col-md-3 card filter ".$groups[$x]['tag_title']."'>";
-		echo "<a id='".$groups[$x]['org_id']."' href='".base_url()."index.php/Group/display/".$groups[$x]['org_id']."'><img class='img-responsive center-cropped' src='";
+		echo "<a id='".$groups[$x]['org_id']."' href='".base_url()."index.php/Group/display/".$groups[$x]['org_id']."'><img class='img-responsive' src='";
 		echo base_url()."uploads/".$groups[$x]['org_picture']."' alt='".$groups[$x]['org_title']."'></a>";
 		echo "<div class='card-block'>";
 		echo "<h5 data-toggle='tooltip' data-placement='top' title='".$groups[$x]['org_title']."' class='card-title' id='card-title' >".$truncatedTitle."</h5>";
